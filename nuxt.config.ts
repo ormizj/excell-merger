52b6d8a0403d6fcb76f700dc752737518667429d
spiderpig60 <spiderpig60@gmail.com>
2024-09-07 00:34:30 +0300

    electron

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-  future: {
-    compatibilityVersion: 4,
-  },
+	modules: ['nuxt-electron'],
+	electron: {
+		build: [
+			{
+				// Main-Process entry file of the Electron App.
+				entry: 'electron/main.ts',
+			},
+		],
+	},
+	compatibilityDate: '2024-04-03',
+	devtools: { enabled: true },
+	future: {
+		compatibilityVersion: 4,
+	},
 });
