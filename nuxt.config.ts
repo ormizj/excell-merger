b22219a7c0aea07cf03e7c375bf25f33b4a37857
spiderpig60 <spiderpig60@gmail.com>
2024-09-07 01:22:01 +0300

    Revert "electron"
    This reverts commit 52b6d8a0403d6fcb76f700dc752737518667429d.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['nuxt-electron'],
-	electron: {
-		build: [
-			{
-				// Main-Process entry file of the Electron App.
-				entry: 'electron/main.ts',
-			},
-		],
-	},
-	compatibilityDate: '2024-04-03',
-	devtools: { enabled: true },
-	future: {
-		compatibilityVersion: 4,
-	},
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
 });
